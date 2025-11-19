d8ea3438283d061b0cb7183bbdd436033ea96193
Klooni1 <klooni123@gmail.com>
2025-11-19 15:23:16 +0200

    Update util.py
    added more errors for incorrect date entries

--- src/util.py
+++ src/util.py
@@ -4,3 +4,8 @@ class UserInputError(Exception):
 def validate_citation(title, author, date):
     if len(title) == 0 or len(author) == 0 or len(date) == 0:
         raise UserInputError("Citation fields cannot be empty")
+    try:
+        if int(date) < 0:
+            raise UserInputError("Date must be a positive number")
+    except ValueError:
+        raise UserInputError("Date must be a number")
