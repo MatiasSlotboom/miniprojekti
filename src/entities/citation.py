d27bafb716be093a80df0317c9205f12db41f9e9
MatiasSlotboom <matias.slotboom@helsinki.fi>
2025-11-20 16:53:06 +0200

    Only use year for citations

--- src/entities/citation.py
+++ src/entities/citation.py
@@ -3,7 +3,7 @@ class Citation:
         self.id = id
         self.title = title
         self.author = author
-        self.date = date
+        self.date = date.year
 
     def __str__(self):
         return f"{self.title} by {self.author}, dated {self.date}"
