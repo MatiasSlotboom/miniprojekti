7d123e9ae134beb5d0dc995ae7149b8e2a27be93
aarokoivula <aaro.koivula@gmail.com>
2025-12-09 21:51:01 +0200

    add more unittests for validate_citation

--- src/tests/validate_citation_test.py
+++ src/tests/validate_citation_test.py
@@ -7,4 +7,16 @@ class TestCitationValidation(unittest.TestCase):
 
     def test_valid_length_does_not_raise_error(self):
         validate_citation("Electronic Commerce", "Michael Merz", "1999", "book")
+    
+    def test_empty_title_raises_error(self):
+        self.assertRaises(UserInputError, validate_citation, "", "Author", "2015", "book")
+    
+    def test_negative_date_raises_error(self):
+        self.assertRaises(UserInputError, validate_citation, "Title", "Author", "-1", "book")
+    
+    def test_non_valid_type_raises_error(self):
+        self.assertRaises(UserInputError, validate_citation, "Title", "Author", "2015", "magazine")
+    
+    def test_date_not_number_raises_error(self):
+        self.assertRaises(UserInputError, validate_citation, "Title", "Author", "another author", "bible")
 
